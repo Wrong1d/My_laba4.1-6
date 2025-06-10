10aaab860f190a7b528312ea2b14779aa08d7338
Wrong1d <Vladimir_stel@mail.ru>
2025-06-10 23:28:06 +0700

    Переход к следующему вопросу и сохранение состояния прошлого

--- app/src/main/java/com/example/my_laba41view/MainActivity.kt
+++ app/src/main/java/com/example/my_laba41view/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-im
+
 
 import android.os.Bundle
 import android.widget.Button
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var questionTextView: TextView
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,11 +37,31 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+
+        savedInstanceState?.let {
+            currentIndex = it.getInt("currentIndex")
+            score = it.getInt("score")
+        }
 
         updateQuestion()
 
         trueButton.setOnClickListener { checkAnswer(true) }
         falseButton.setOnClickListener { checkAnswer(false) }
+
+        nextButton.setOnClickListener {
+            currentIndex++
+            if (currentIndex < questions.size) {
+                updateQuestion()
+                enableAnswerButtons()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("currentIndex", currentIndex)
+        outState.putInt("score", score)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
@@ -59,5 +80,10 @@ class MainActivity : AppCompatActivity() {
     private fun disableAnswerButtons() {
         trueButton.isEnabled = false
         falseButton.isEnabled = false
+    }
+
+    private fun enableAnswerButtons() {
+        trueButton.isEnabled = true
+        falseButton.isEnabled = true
     }
 }
