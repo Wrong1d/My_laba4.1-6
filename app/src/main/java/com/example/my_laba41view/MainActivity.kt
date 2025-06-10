6bf933f6986246854874232be09c7886de0bdb5e
Wrong1d <Vladimir_stel@mail.ru>
2025-06-10 22:52:59 +0700

    Графический интерфейс и подключение библиотек

---
+++ app/src/main/java/com/example/my_laba41view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.my_laba41view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.app.AlertDialog
+import android.widget.Button
+import android.widget.TextView
+
+
