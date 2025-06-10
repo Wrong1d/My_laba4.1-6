3bb023bf10256ddd5ded8289a5fb46d296c7a07d
Wrong1d <Vladimir_stel@mail.ru>
2025-06-10 23:21:42 +0700

    Обработка ответов пользователя

--- app/src/main/java/com/example/my_laba41view/MainActivity.kt
+++ app/src/main/java/com/example/my_laba41view/MainActivity.kt
@@ -10,7 +10,7 @@ im
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,20 +20,44 @@ class MainActivity : AppCompatActivity() {
         "Australia is an island continent."
     )
 
+    private val correctAnswers = listOf(true, true, true)
+
     private var currentIndex = 0
+    private var score = 0
 
     private lateinit var questionTextView: TextView
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         questionTextView = findViewById(R.id.question_text_view)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
 
         updateQuestion()
+
+        trueButton.setOnClickListener { checkAnswer(true) }
+        falseButton.setOnClickListener { checkAnswer(false) }
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = correctAnswers[currentIndex]
+        if (userAnswer == correctAnswer) {
+            score++
+        }
+
+        disableAnswerButtons()
     }
 
     private fun updateQuestion() {
         questionTextView.text = questions[currentIndex]
+    }
+
+    private fun disableAnswerButtons() {
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
     }
 }
